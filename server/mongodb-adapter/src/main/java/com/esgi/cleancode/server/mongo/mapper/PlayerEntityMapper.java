06e08d6c3e0aca140a02c9e1598f9824dfafd89c
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-02-27 22:49:45 +0100

    fix(server): entity & mapper to open pack

--- server/mongodb-adapter/src/main/java/com/esgi/cleancode/server/mongo/mapper/PlayerEntityMapper.java
+++ server/mongodb-adapter/src/main/java/com/esgi/cleancode/server/mongo/mapper/PlayerEntityMapper.java
@@ -1,9 +1,14 @@
 package com.esgi.cleancode.server.mongo.mapper;
 
+import java.util.Optional;
+
 import com.esgi.cleancode.domain.functional.model.Player;
+import com.esgi.cleancode.server.mongo.entity.DeckEntity;
 import com.esgi.cleancode.server.mongo.entity.PlayerEntity;
+import com.esgi.cleancode.server.mongo.repository.DeckRepository;
 
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 
 public interface PlayerEntityMapper {
     
@@ -20,16 +25,26 @@ public interface PlayerEntityMapper {
         return List.ofAll(entities.map(entity -> toDomain(entity)));
     }
 
-    static PlayerEntity fromDomain(Player player) {
-        return PlayerEntity.builder()
-            .id(player.getId())
-            .name(player.getName())
-            .balance(player.getBalance())
-            .deck(DeckEntityMapper.fromDomain(player.getDeck()))
-            .build();
+    static PlayerEntity fromDomain(Player player, DeckRepository deckRepository) {
+        PlayerEntity entity = new PlayerEntity();
+        Optional<DeckEntity> deckEntity = deckRepository.findById(player.getDeck().getId());
+        
+        entity.setId(player.getId());
+        entity.setName(player.getName());
+        entity.setBalance(player.getBalance());
+        
+        if (deckEntity.isPresent())
+            entity.setDeck(deckEntity.get());
+        else {
+            DeckEntity newDeck = DeckEntityMapper.fromDomain(player.getDeck());
+            entity.setDeck(newDeck);
+            deckRepository.save(newDeck);
+        }
+
+        return entity;
     }
 
     static List<PlayerEntity> fromDomain(List<Player> players) {
-        return List.ofAll(players.map(player -> fromDomain(player)));
+        return List.ofAll(players.map(player -> fromDomain(player, null)));
     }
 }
